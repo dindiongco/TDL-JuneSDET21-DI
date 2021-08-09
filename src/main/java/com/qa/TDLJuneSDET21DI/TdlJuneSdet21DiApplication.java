fbeeac4d30cbfd5f4d9b2082740373579335b2a7
dindiongco <dindiongco@qa.com>
2021-08-09 10:45:47 +0100

    DI: Initial commit.

---
+++ src/main/java/com/qa/TDLJuneSDET21DI/TdlJuneSdet21DiApplication.java
@@ -0,0 +1,13 @@
+package com.qa.TDLJuneSDET21DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlJuneSdet21DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlJuneSdet21DiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/TDLJuneSDET21DI/TdlJuneSdet21DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.TDLJuneSDET21DI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlJuneSdet21DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
